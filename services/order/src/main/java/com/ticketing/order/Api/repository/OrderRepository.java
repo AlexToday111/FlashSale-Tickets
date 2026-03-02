6622a84a25977d262c59d809c22a73ab637837a1
AlexToday111 <ernestkudakaev6@mail.ru>
2026-03-02 17:14:07 +0300

    chore(order): repo

---
+++ services/order/src/main/java/com/ticketing/order/Api/repository/OrderRepository.java
@@ -0,0 +1,12 @@
+package com.ticketing.order.Api.repository;
+
+import com.ticketing.order.Api.model.OrderEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface OrderRepository extends JpaRepository<OrderEntity, UUID> {
+    Optional<OrderEntity> findByReservationId(UUID reservationId);
+    boolean existsByReservationId(UUID reservationId);
+}
